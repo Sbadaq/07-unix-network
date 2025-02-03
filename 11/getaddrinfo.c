76656ee29b409a07ba4826ebd8bd35a048ba1336
haolaoshi <liujiahao.oak@gmail.com>
2025-02-04 01:23:44 +0800

    20250204 初七,凌晨

---
+++ 11/getaddrinfo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int main() {
+    struct addrinfo hints, *res;
+    int status;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; // IPv4 或 IPv6
+    hints.ai_socktype = SOCK_STREAM; // TCP 套接字
+
+    status = getaddrinfo("www.example.com", "http", &hints, &res);
+    if (status != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        return 1;
+    }
+
+    // 使用 res 中的地址信息
+    // ...
+
+    freeaddrinfo(res); // 释放 addrinfo 链表
+    return 0;
+}
